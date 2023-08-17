ad89433ce644e4390e2a7f194ee276df464cbf71
datajcthemax <data.jcthemax@gmail.com>
2023-08-17 14:10:25 +0900

    react native start

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
+
+const Home = () => {
+    <View>
+        <Text>Home</Text>
+    </View>
+}
+
+export default Home;
